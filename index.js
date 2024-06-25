9acac34f663b1e9c3c0826c3f42c025d6f4abf6d
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-25 17:02:19 +0500

    Readonly in TS

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello, world");
+// ! Readonly Type
+var user = {
+    _id: "12345",
+    name: "Hammad",
+    email: "h@gmail.com",
+    isActive: false,
+};
+user.email = "hammad@gmail.com";
+// user._id = "123"; // error -- readonly property
--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello, world");
+// ! Readonly Type
+
+type User = {
+	readonly _id: string; // Cannot be changed
+	name: string;
+	email: string;
+	isActive: boolean;
+};
+
+let user: User = {
+	_id: "12345",
+	name: "Hammad",
+	email: "h@gmail.com",
+	isActive: false,
+};
+
+user.email = "hammad@gmail.com";
+// user._id = "123"; // error -- readonly property
