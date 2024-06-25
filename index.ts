81b2f4c9013ed9f028e4ab6f102d4bc1fc6ea6cd
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-25 18:33:03 +0500

    Interfaces in TS

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log("Hello, world");
+// ! Interfaces
+
+// interface User {
+// 	readonly dbId: number;
+// 	userId: number;
+// 	name: string;
+// 	googleId?: string;
+// 	// startTrial: () => string;
+// 	startTrial(): string;
+// 	getSomething(val: number): number;
+// }
+
+// const hammad: User = {
+// 	dbId: 1213232,
+// 	userId: 1,
+// 	name: "Hammad",
+// 	startTrial: () => "Trial",
+// 	getSomething: (val: 1) => {
+// 		return val;
+// 	},
+// };
+// // hammad.dbId = 1; // error
