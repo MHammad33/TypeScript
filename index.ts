08362b12640db51a1d7d392b8545f2db6cb62305
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-25 18:38:53 +0500

    Interfaces vs Types

--- index.ts
+++ index.ts
@@ -20,3 +20,39 @@
 // 	},
 // };
 // // hammad.dbId = 1; // error
+
+// ! Interfaces vs Types
+// Interfaces automatically inherits properties when declared with same name
+
+interface User {
+	readonly dbId: number;
+	userId: number;
+	name: string;
+	googleId?: string;
+	// startTrial: () => string;
+	startTrial(): string;
+	getSomething(val: number): number;
+}
+
+interface User {
+	// * All properties inherited
+	githubToken: string;
+}
+
+const hammad: User = {
+	dbId: 1213232,
+	userId: 1,
+	name: "Hammad",
+	startTrial: () => "Trial",
+	getSomething: (val: 1) => {
+		return val;
+	},
+	githubToken: "8738fbckzs",
+};
+
+// ? Inheritance
+interface Admin extends User {
+	role: "admin" | "ta" | "learner";
+}
+
+// const Ali: Admin = hammad;
