a823692062529ea0f95af5edc5e7b929fd5ed444
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-25 17:06:17 +0500

    Multiple Types

--- index.ts
+++ index.ts
@@ -18,17 +18,26 @@
 // // user._id = "123"; // error -- readonly property
 
 // ! Optional
-type User = {
-	readonly _id: string; // Cannot be changed
-	name: string;
-	email: string;
-	isActive: boolean;
-	creditCardsDetail?: string;
-};
+// type User = {
+// 	readonly _id: string; // Cannot be changed
+// 	name: string;
+// 	email: string;
+// 	isActive: boolean;
+// 	creditCardsDetail?: string;
+// };
 
-let user: User = {
-	_id: "12345",
-	name: "Hammad",
-	email: "h@gmail.com",
-	isActive: false,
-};
+// let user: User = {
+// 	_id: "12345",
+// 	name: "Hammad",
+// 	email: "h@gmail.com",
+// 	isActive: false,
+// };
+
+// ! Multiple Types
+type cardNumber = { cardNumber: string };
+type cardDate = { cardDate: string };
+
+type cardDetails = cardNumber &
+	cardDate & {
+		cvv: number;
+	};
