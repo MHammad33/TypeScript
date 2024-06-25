e3d0f8a85780c95dc64b65b55bc57cb6b38b25a0
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-25 19:11:46 +0500

    Private and Public

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 // ! Classes
+// class User {
+// 	name: string;
+// 	password: number;
+// 	readonly city: string = "Pasrur";
+// 	constructor(name: string, password: number) {
+// 		this.name = name;
+// 		this.password = password;
+// 	}
+// }
+// const hammad = new User("Hammad", 1122);
+// hammad.city = "Pasrur"; // error
+// ! Private and Public in TypeScript
 var User = /** @class */ (function () {
     function User(name, password) {
+        this.name = name;
+        this.password = password;
         this.city = "Pasrur";
         this.name = name;
         this.password = password;
@@ -9,3 +23,4 @@ var User = /** @class */ (function () {
 }());
 var hammad = new User("Hammad", 1122);
 // hammad.city = "Pasrur"; // error
+// console.log(hammad.city); // error
--- index.ts
+++ index.ts
@@ -12,3 +12,17 @@
 
 // const hammad = new User("Hammad", 1122);
 // hammad.city = "Pasrur"; // error
+
+// ! Private and Public in TypeScript
+
+class User {
+	private readonly city: string = "Pasrur";
+	constructor(public name: string, private password: number) {
+		this.name = name;
+		this.password = password;
+	}
+}
+
+const hammad = new User("Hammad", 1122);
+// hammad.city = "Pasrur"; // error
+// console.log(hammad.city); // error
