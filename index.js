82b9bd771fd286665cf3d8ad1a3e05b1fc7b70b4
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-25 18:11:04 +0500

    Union Type and Constants in TS

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello, world");
+// ! Unions
+var Hammad = { name: "Hammad", id: 22 };
+Hammad = { username: "Hammad", id: 22 };
+function getUser(id) {
+    // id.toLowercase(); -- error
+    if (typeof id === "string") {
+        id.toLowerCase();
+    }
+}
+getUser(1);
+getUser("Hammad");
--- index.ts
+++ index.ts
@@ -1,1 +1,40 @@
-console.log("Hello, world");
+// ! Unions
+
+// let score: number | string = 33;
+
+// score = 44;
+// score = "55"; // No error
+// // score = false; // error
+
+// ? Type Aliases for Union
+// type User = {
+// 	name: string;
+// 	id: number;
+// };
+
+// type Admin = {
+// 	username: string;
+// 	id: number;
+// };
+
+// let Hammad: User | Admin = { name: "Hammad", id: 22 };
+// Hammad = { username: "Hammad", id: 22 };
+
+// ? Interesting error
+// function getUser(id: number | string): void {
+// 	// id.toLowercase(); -- error
+// 	if (typeof id === "string") {
+// 		id.toLowerCase();
+// 	}
+// }
+// getUser(1);
+// getUser("Hammad");
+
+// ? Union Types in Arrays
+// const data: number[] | string[] = [1, 2, 3, "4"]; // Not allowed
+// * Correct Syntax
+// const data: (number | string)[] = [1, 2, 3, "4"];
+
+// ? Constant Assignments
+let seatAllotment: "aisle" | "middle" | "window";
+seatAllotment = "middle";
