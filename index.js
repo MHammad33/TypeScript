47680a19d80a2b5b47b777639bda8a709443878f
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-25 16:39:18 +0500

    Return Values types

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 // ! Functions
 // ? Types in parameters
-function addTwo(num) {
-    return num + 2;
-}
-addTwo(2);
-function getUpper(val) {
-    return val.toUpperCase();
-}
-getUpper("hammad");
-function signUpUser(name, email, password) { }
-signUpUser("Hammad", "hammad@gmail.com", "1122");
-var loginUser = function (name, isAuthorized) {
-    if (isAuthorized === void 0) { isAuthorized = false; }
-};
-loginUser("Hammad");
+// function addTwo(num: number) {
+// 	return num + 2;
+// }
+// addTwo(2);
+// function getUpper(val: string) {
+// 	return val.toUpperCase();
+// }
+// getUpper("hammad");
+// function signUpUser(name: string, email: string, password: string) {}
+// signUpUser("Hammad", "hammad@gmail.com", "1122");
+// const loginUser = (name: string, isAuthorized: boolean = false) => {};
+// loginUser("Hammad");
+// ? Return Values
+// const getUpper = (val: string) => val.toUpperCase();
+// const upper = getUpper("hammad"); // String type
+// const getVal = (myVal: number): boolean => {
+// 	if (myVal > 5) {
+// 		return true;
+// 	}
+// 	return false;
+// };
+// console.log(getVal(4));
+var heroes = ["hero1", "hero2", "hero3"];
+// const heroes = [1, 2, 3];
+// ? Not a big difference
+// heroes.map((hero: string) => {
+// 	return `Hero is: ${hero}`;
+// });
+// ? Better Code
+// heroes.map((hero): string => {
+// 	return `Hero is: ${hero}`;
+// });
--- index.ts
+++ index.ts
@@ -16,3 +16,28 @@
 
 // const loginUser = (name: string, isAuthorized: boolean = false) => {};
 // loginUser("Hammad");
+
+// ? Return Values
+// const getUpper = (val: string) => val.toUpperCase();
+// const upper = getUpper("hammad"); // String type
+
+// const getVal = (myVal: number): boolean => {
+// 	if (myVal > 5) {
+// 		return true;
+// 	}
+// 	return false;
+// };
+// console.log(getVal(4));
+
+const heroes = ["hero1", "hero2", "hero3"];
+// const heroes = [1, 2, 3];
+
+// ? Not a big difference
+// heroes.map((hero: string) => {
+// 	return `Hero is: ${hero}`;
+// });
+
+// ? Better Code
+// heroes.map((hero): string => {
+// 	return `Hero is: ${hero}`;
+// });
