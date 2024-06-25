ca3483b7f1245be9fed251b9c9b22036e771bcc8
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-25 16:23:46 +0500

    Any Type

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello, world");
+// ! any type
+// ? Not better
+// let hero; // * Type -> any
+// function getHero() {
+// 	return hero;
+// }
+// hero = getHero();
+// ? Better Code
+var hero; // * Type -> any
+function getHero() {
+    return "2";
+}
+hero = getHero();
--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello, world");
+// ! any type
+
+// ? Not better
+// let hero; // * Type -> any
+// function getHero() {
+// 	return hero;
+// }
+// hero = getHero();
+
+// ? Better Code
+// let hero: string; // * Type -> any
+// function getHero() {
+// 	return "2";
+// }
+// hero = getHero();
