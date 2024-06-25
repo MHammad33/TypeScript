4348d7f972b5163370737513224f97b9322788b0
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-25 19:05:32 +0500

    Intro to Classes in TS

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello, world");
+// ! Classes
+var User = /** @class */ (function () {
+    function User(name, password) {
+        this.city = "Pasrur";
+        this.name = name;
+        this.password = password;
+    }
+    return User;
+}());
+var hammad = new User("Hammad", 1122);
+// hammad.city = "Pasrur"; // error
--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello, world");
+// ! Classes
+
+// class User {
+// 	name: string;
+// 	password: number;
+// 	readonly city: string = "Pasrur";
+// 	constructor(name: string, password: number) {
+// 		this.name = name;
+// 		this.password = password;
+// 	}
+// }
+
+// const hammad = new User("Hammad", 1122);
+// hammad.city = "Pasrur"; // error
