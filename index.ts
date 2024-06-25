55d14e719447c637188eb12fec7390acadafbfda
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-25 16:52:18 +0500

    Objects

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-console.log("Hello, world");
+// ! Objects
+
+// const User = {
+// 	name: "Hammad",
+// 	email: "hammad@gmail.com",
+// 	isActive: false,
+// };
+
+// ? Objects in Parameters
+// function createUser({ name: string, isPaid: boolean }) {
+// 	return {};
+// }
+// createUser({ name: "Hammad", isPaid: false });
+
+// ? Returning an object
+// const returnObj = (): { name: string; price: number } => {
+// 	return { name: "Hammad", price: 12 };
+// };
+
+// ? Odd Behavior of Objects
+// function createUser({ name: string, isPaid: boolean }) {
+// 	return {};
+// }
+// let user = { name: "Hammad", isPaid: false, email: "hammad@gmail.com" };
+// createUser(user);
