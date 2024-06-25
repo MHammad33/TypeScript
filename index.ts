321aa6b77504732e9d7dc11ee4c4f2702acacf98
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-25 17:03:26 +0500

    Optional in TS

--- index.ts
+++ index.ts
@@ -1,10 +1,29 @@
 // ! Readonly Type
 
+// type User = {
+// 	readonly _id: string; // Cannot be changed
+// 	name: string;
+// 	email: string;
+// 	isActive: boolean;
+// };
+
+// let user: User = {
+// 	_id: "12345",
+// 	name: "Hammad",
+// 	email: "h@gmail.com",
+// 	isActive: false,
+// };
+
+// user.email = "hammad@gmail.com";
+// // user._id = "123"; // error -- readonly property
+
+// ! Optional
 type User = {
 	readonly _id: string; // Cannot be changed
 	name: string;
 	email: string;
 	isActive: boolean;
+	creditCardsDetail?: string;
 };
 
 let user: User = {
@@ -13,6 +32,3 @@ let user: User = {
 	email: "h@gmail.com",
 	isActive: false,
 };
-
-user.email = "hammad@gmail.com";
-// user._id = "123"; // error -- readonly property
