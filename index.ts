f12aa2b16de2373109148279545a4415fff34be1
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-25 16:20:26 +0500

    Number, String, Boolean Data Type

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello, world");
+// ! Strings
+// const str: string = "3";
+// console.log(str);
+
+// ! Numbers
+// let num: number = 2;
+// console.log(num);
+
+// ! Boolean
+// const isLoggedIn: boolean = false;
+// console.log(isLoggedIn);
+
+// ! Type Inferences
+// let num: number = 4;
+// let num = 4; // * Better code as TS is smart enough to know it's type
