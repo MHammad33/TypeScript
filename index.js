bb8f3bac741eb37c1aabafd0b6a2a6a186b444d8
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-25 16:32:08 +0500

    Parameter Types

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello, world");
+// ! Functions
+// ? Types in parameters
+function addTwo(num) {
+    return num + 2;
+}
+addTwo(2);
+function getUpper(val) {
+    return val.toUpperCase();
+}
+getUpper("hammad");
+function signUpUser(name, email, password) { }
+signUpUser("Hammad", "hammad@gmail.com", "1122");
+var loginUser = function (name, isAuthorized) {
+    if (isAuthorized === void 0) { isAuthorized = false; }
+};
+loginUser("Hammad");
--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello, world");
+// ! Functions
+
+// ? Types in parameters
+// function addTwo(num: number) {
+// 	return num + 2;
+// }
+// addTwo(2);
+
+// function getUpper(val: string) {
+// 	return val.toUpperCase();
+// }
+// getUpper("hammad");
+
+// function signUpUser(name: string, email: string, password: string) {}
+// signUpUser("Hammad", "hammad@gmail.com", "1122");
+
+// const loginUser = (name: string, isAuthorized: boolean = false) => {};
+// loginUser("Hammad");
