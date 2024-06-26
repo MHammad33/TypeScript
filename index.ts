483f578b55729916619ff5026c0f5ef37f6c9b30
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-26 18:04:28 +0500

    Generics Basics

--- index.ts
+++ index.ts
@@ -1,1 +1,40 @@
-console.log("Hello, world");
+// ! Generics
+
+// const score: Array<number> = [];
+// const names: Array<string> = [];
+
+// ? Examples
+// function identityOne(val: boolean | number): boolean | number {
+// 	return val;
+// }
+
+// * No idea of type here
+// function identityTwo(val: any): any {
+// 	return val;
+// }
+
+// * Type
+// function identityThree<Type>(val: Type): Type {
+// 	return val;
+// }
+
+// identityThree(2);
+// identityThree("str");
+
+// * Easy to write Code
+// function identityFour<T>(val: T): T {
+// 	return val;
+// }
+// identityFour(2);
+// identityFour("2");
+
+// * Interfaces
+// interface Bottle {
+// 	brand: string;
+// 	type: number;
+// }
+
+// function identityFive<Bottle>(val: Bottle): Bottle {
+// 	return val;
+// }
+// identityFive<Bottle>({});
