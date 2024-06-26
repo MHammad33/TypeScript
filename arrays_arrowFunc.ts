d456a3f10f53a6abfc0e633a6bdc988b212b0e7e
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-26 18:10:11 +0500

    Arrays and Arrow Functions in Generics

---
+++ arrays_arrowFunc.ts
@@ -0,0 +1,25 @@
+// ! Arrays
+
+// function getSearchedProduct<T>(products: T[]): T {
+// 	// return 3; // error
+
+// 	//  Do Some DB operations
+// 	const index = 3;
+// 	return products[index];
+// }
+
+// ! Arrow Functions
+
+// const getMoreSearchedProduct = <T>(products: T[]): T => {
+// 	// Do Some DB Operations
+// 	const index = 3;
+// 	return products[index];
+// };
+
+// * Some more examples
+// comma in <T, >
+// const getMoreSearchedProduct = <T,>(products: T[]): T => {
+// 	// Do Some DB Operations
+// 	const index = 3;
+// 	return products[index];
+// };
